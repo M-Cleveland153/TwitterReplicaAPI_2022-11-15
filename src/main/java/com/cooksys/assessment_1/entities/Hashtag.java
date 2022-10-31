865d3fa6367fd647944b5de9f21aebf20867cee0
M-Cleveland153 <m999cleveland@gmail.com>
2022-10-31 16:03:00 -0500

    commiting for the first time to michaelsBranch.

---
+++ src/main/java/com/cooksys/assessment_1/entities/Hashtag.java
@@ -0,0 +1,27 @@
+package com.cooksys.assessment_1.entities;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Hashtag {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	private String label;
+	
+	private Timestamp firstUsed;
+	
+	private Timestamp lastUsed;
+
+}
