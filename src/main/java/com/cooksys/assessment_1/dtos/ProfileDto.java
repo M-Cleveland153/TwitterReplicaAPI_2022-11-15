b38c800827c1e946e7e8852aa58253f99db0ca26
aobispo123 <andreobispo123@gmail.com>
2022-11-01 13:57:31 -0700

    Created CredentialsDto and ProfileDto

--- src/main/java/com/cooksys/assessment_1/dtos/ProfileDto.java
+++ src/main/java/com/cooksys/assessment_1/dtos/ProfileDto.java
@@ -1,13 +1,13 @@
 package com.cooksys.assessment_1.dtos;
 
-import com.cooksys.assessment_1.entities.Credentials;
+import com.cooksys.assessment_1.entities.Profile;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Data
-public class CredentialsDto {
+public class ProfileDto {
 	
-	private Credentials credentials;
+	private Profile profile;
 }
